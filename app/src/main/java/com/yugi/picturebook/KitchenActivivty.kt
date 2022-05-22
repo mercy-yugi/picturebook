31ee936891eeed827e8d2c6ad09a60a5aa9ff874
mercy-yugi <mercyyugi@gmail.com>
2022-05-23 01:03:26 +0300

    first commit

--- app/src/main/java/com/yugi/picturebook/KitchenActivivty.kt
+++ app/src/main/java/com/yugi/picturebook/KitchenActivivty.kt
@@ -1,11 +1,26 @@
 package com.yugi.picturebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class KitchenActivivty : AppCompatActivity() {
+    lateinit var btnBack4: Button
+    lateinit var btnNext4: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kitchen_activivty)
+
+        btnBack4 = findViewById(R.id.btnBack4)
+        btnBack4.setOnClickListener {
+            val intent = Intent(this, DinningActivity::class.java)
+            startActivity(intent)
+        }
+        btnNext4 = findViewById(R.id.btnNext3)
+        btnNext4.setOnClickListener {
+            val intent = Intent(this, OutdoorActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
