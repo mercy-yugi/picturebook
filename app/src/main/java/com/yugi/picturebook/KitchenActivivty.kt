09013faf9f76f9953d5f052e34758d689725fcb5
mercy-yugi <mercyyugi@gmail.com>
2022-05-23 08:37:04 +0300

    THIRD commit

--- app/src/main/java/com/yugi/picturebook/KitchenActivivty.kt
+++ app/src/main/java/com/yugi/picturebook/KitchenActivivty.kt
@@ -6,8 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 
 class KitchenActivivty : AppCompatActivity() {
-    lateinit var btnBack4: Button
-    lateinit var btnNext4: Button
+lateinit var btnBack4:Button
+lateinit var btnNext4:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kitchen_activivty)
@@ -17,9 +17,9 @@ class KitchenActivivty : AppCompatActivity() {
             val intent = Intent(this, DinningActivity::class.java)
             startActivity(intent)
         }
-        btnNext4 = findViewById(R.id.btnNext3)
+        btnNext4 = findViewById(R.id.btnNext4)
         btnNext4.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, OutdoorActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/yugi/picturebook/OutdoorActivity.kt
+++ app/src/main/java/com/yugi/picturebook/OutdoorActivity.kt
@@ -6,24 +6,17 @@ import android.os.Bundle
 import android.widget.Button
 
 class OutdoorActivity : AppCompatActivity() {
-    lateinit var btnBack5:Button
-    lateinit var btnNext5:Button
+   lateinit var btnBack5:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_outdoor)
 
         btnBack5 = findViewById(R.id.btnBack5)
         btnBack5.setOnClickListener {
-            val intent = Intent(this, DinningActivity::class.java)
-            startActivity(intent)
-        }
-
-        btnNext5= findViewById(R.id.btnNext1)
-        btnNext5.setOnClickListener {
             val intent = Intent(this, KitchenActivivty::class.java)
             startActivity(intent)
         }
-
 
     }
 }
